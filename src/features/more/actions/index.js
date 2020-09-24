4079cbb39ea0449809bc16ac17f3a4cff2c8494a
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2020-09-24 20:36:54 +0200

    Removed features from the more directory.

--- src/features/more/actions/index.js
+++ src/features/more/actions/index.js
@@ -1,17 +1,6 @@
 import {
     MORE_TRIGGER_SPINNER,
     LOGOUT,
-    GET_ITEMS,
-    SET_ITEMS,
-    ITEM_ADD,
-    CLEAR_ITEM,
-    GET_EDIT_ITEM,
-    REMOVE_ITEM,
-    ITEM_EDIT,
-    SET_ITEM,
-    DELETE_ITEM,
-    GENERATE_REPORT,
-    SET_FILTER_ITEMS
 } from "../constants";
 
 export const moreTriggerSpinner = (payload) => ({
@@ -21,62 +10,5 @@ export const moreTriggerSpinner = (payload) => ({
 
 export const logout = (payload) => ({
     type: LOGOUT,
-    payload,
-});
-
-export const addItem = (payload = {}) => ({
-    type: ITEM_ADD,
-    payload,
-});
-
-export const editItem = (payload = {}) => ({
-    type: ITEM_EDIT,
-    payload,
-});
-
-export const getItems = (payload = {}) => ({
-    type: GET_ITEMS,
-    payload,
-});
-
-export const removeItem = (payload = {}) => ({
-    type: REMOVE_ITEM,
-    payload,
-});
-
-export const setItems = (payload = {}) => ({
-    type: SET_ITEMS,
-    payload,
-});
-
-export const setFilterItems = (payload = {}) => ({
-    type: SET_FILTER_ITEMS,
-    payload,
-});
-
-export const setItem = (payload = {}) => ({
-    type: SET_ITEM,
-    payload,
-});
-
-export const clearItem = (payload = {}) => ({
-    type: CLEAR_ITEM,
-    payload,
-});
-
-export const deleteItem = (payload = {}) => ({
-    type: DELETE_ITEM,
-    payload,
-});
-
-export const getEditItem = (payload = {}) => ({
-    type: GET_EDIT_ITEM,
-    payload,
-});
-
-
-// Reports
-export const generateReport = (payload) => ({
-    type: GENERATE_REPORT,
     payload,
 });
--- src/features/more/reducers/index.js
+++ src/features/more/reducers/index.js
@@ -1,22 +1,12 @@
 import {
     MORE_TRIGGER_SPINNER,
-    SET_ITEMS,
-    CLEAR_ITEM,
-    SET_ITEM,
-    DELETE_ITEM,
-    SET_FILTER_ITEMS,
 } from '../constants';
 import { env } from '../../../config';
 
 const initialState = {
     loading: {
         logoutLoading: false,
-        itemsLoading: false,
-        itemLoading: false,
     },
-    items: [],
-    filterItems: [],
-    item: null
 };
 
 export default function moreReducer(state = initialState, action) {
@@ -25,45 +15,6 @@ export default function moreReducer(state = initialState, action) {
     switch (type) {
         case MORE_TRIGGER_SPINNER:
             return { ...state, loading: { ...state.loading, ...payload } };
-
-        case SET_ITEMS:
-
-            const { items, fresh, prepend } = payload;
-
-            if (prepend) {
-                return { ...state, items: [...items, ...state.items] };
-            }
-
-            if (!fresh) {
-                return { ...state, items: [...state.items, ...items] };
-            }
-
-            return { ...state, items };
-
-        case SET_FILTER_ITEMS:
-
-            if (!payload.fresh) {
-                return {
-                    ...state,
-                    filterItems: [...state.filterItems, ...payload.items]
-                };
-            }
-
-            return { ...state, filterItems: payload.items };
-
-
-        case DELETE_ITEM:
-            const { id } = payload
-
-            const remainItems = state.items.filter(val => val.id !== id)
-
-            return { ...state, items: remainItems };
-
-        case CLEAR_ITEM:
-            return { ...state, item: null };
-
-        case SET_ITEM:
-            return { ...state, item: payload.item };
 
         default:
             return state;
