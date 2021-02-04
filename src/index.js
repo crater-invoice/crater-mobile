6d69167728927ace7184e9004e396c5aaa43834b
KomelAbbas <komelabbasweb786@gmail.com>
2021-02-04 18:30:39 +0530

    Check OTA (Over-the-Air) Update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { NavigationActions } from 'react-navigation';
 import { store, persistor } from './store';
 import ApplicationNavigator from './navigation/containers';
-import { getBootstrap, getAppVersion } from './features/authentication/actions';
+import { checkOTAUpdate } from './features/authentication/actions';
 import { env } from '@/config';
-import { ROUTES } from '@/navigation';
-import { hasValue, loadFonts } from './constants';
+import { loadFonts } from './constants';
 
 console.disableYellowBox = true;
 console.warn = () => {};
@@ -27,42 +25,10 @@ export default class Root extends Component<{}, IState> {
         loadFonts({
             afterLoad: () => {
                 this.setState({ fontLoaded: true });
-
-                const reduxStore = store.getState();
-                const { idToken = null } = reduxStore?.auth;
-                const { endpointApi = null } = reduxStore?.global;
-
-                if (idToken) {
-                    store.dispatch(getBootstrap());
-                }
-
-                this.checkAppVersion(endpointApi);
+                store.dispatch(checkOTAUpdate());
             }
         });
     }
-
-    checkAppVersion = endpointApi => {
-        if (!hasValue(endpointApi)) {
-            return;
-        }
-
-        store.dispatch(
-            getAppVersion({
-                onResult: ({ version }) => {
-                    if (
-                        version &&
-                        parseInt(env.APP_VERSION) < parseInt(version)
-                    ) {
-                        store.dispatch(
-                            NavigationActions.navigate({
-                                routeName: ROUTES.UPDATE_APP_VERSION
-                            })
-                        );
-                    }
-                }
-            })
-        );
-    };
 
     render() {
         const { fontLoaded } = this.state;
