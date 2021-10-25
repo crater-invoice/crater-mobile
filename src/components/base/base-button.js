766dac80fec2fe5755c1dae9b5a1b0eb9c5af779
KomelAbbas <komelabbasweb786@gmail.com>
2021-10-25 16:53:29 +0530

    update base button component props.

--- src/components/base/base-button.js
+++ src/components/base/base-button.js
@@ -39,7 +39,12 @@ interface IProps {
   /**
    * Type of button.
    */
-  type?: 'primary' | 'danger' | 'primary-gradient';
+  type?:
+    | 'primary'
+    | 'primary-gradient'
+    | 'primary-outline'
+    | 'danger'
+    | 'danger-outline';
 
   /**
    * Size of button.
@@ -62,6 +67,23 @@ interface IProps {
   style?: StyleProp<ViewStyle> | undefined;
 }
 
+const bgColor = {
+  primary: {
+    light: colors.primary,
+    dark: colors.primaryLight
+  },
+  danger: {
+    light: colors.danger,
+    dark: colors.danger
+  }
+};
+
+const height = {
+  sm: defineLargeSizeParam(32, 30),
+  md: defineLargeSizeParam(42, 40),
+  lg: defineLargeSizeParam(44, 42)
+};
+
 export const Button = (props: IProps) => {
   const {
     onPress,
@@ -72,25 +94,9 @@ export const Button = (props: IProps) => {
     size = 'md',
     style
   } = props;
-
-  const bgColor = {
-    primary: {
-      light: colors.primary,
-      dark: colors.primaryLight
-    },
-    danger: {
-      light: colors.danger,
-      dark: colors.danger
-    }
-  };
-  const height = {
-    sm: defineLargeSizeParam(33, 30),
-    md: defineLargeSizeParam(41, 39),
-    lg: defineLargeSizeParam(44, 42)
-  };
-
+  const isOutline = type.includes('outline');
   const label = (
-    <Text white h5 medium>
+    <Text h5 white medium style={isOutline && styles.outlineText(props)}>
       {props.children}
     </Text>
   );
@@ -115,6 +121,7 @@ export const Button = (props: IProps) => {
         style={style}
         disabled={disabled || loading}
         opacity={disabled ? 0.7 : 1}
+        style={isOutline && styles.outlineView(props)}
       >
         {type === 'primary-gradient' ? (
           <LinearGradient {...gradientStyle}>{children}</LinearGradient>
@@ -164,9 +171,9 @@ export const BaseButtonGroup = props => {
 const Container = styled(RNView)`
   background-color: ${props => props.theme?.secondaryBgColor};
   border-color: ${props => props.theme?.input?.borderColor};
-  padding-vertical: 15;
   padding-horizontal: 10;
-  padding-bottom: ${defineSize(14, 14, 14, 35)};
+  padding-top: ${defineSize(16, 16, 16, 18)};
+  padding-bottom: ${defineSize(15, 15, 15, 35)};
   border-top-width: 1;
 `;
 
@@ -175,11 +182,20 @@ const Row = styled(RNView)`
   justify-content: space-between;
 `;
 
+const outlineType = type => (type ? type.split('-')[0] : 'primary');
+
 const styles = StyleSheet.create({
   gradient: {
     width: '100%',
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  outlineView: ({type, theme}) => ({
+    borderWidth: 1,
+    borderColor: bgColor?.[outlineType(type)]?.[theme?.mode]
+  }),
+  outlineText: ({type, theme}) => ({
+    color: bgColor?.[outlineType(type)]?.[theme?.mode]
+  })
 });
