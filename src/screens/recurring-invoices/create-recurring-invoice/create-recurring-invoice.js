d925d4cbc574dd1dae79c5c956b7f451609d219f
http127001com <http.127001.com@gmail.com>
2021-10-29 15:30:03 +0530

    Fix date format in recurring-invoice

--- src/screens/recurring-invoices/create-recurring-invoice/create-recurring-invoice.js
+++ src/screens/recurring-invoices/create-recurring-invoice/create-recurring-invoice.js
@@ -43,6 +43,7 @@ import {
 } from '@/components/final-amount/final-amount-calculation';
 import {setCalculationRef} from 'stores/common/helpers';
 import {getApiFormattedCustomFields, secondaryHeader} from '@/utils';
+import moment from 'moment';
 
 export default class CreateRecurringInvoice extends Component<IProps, IStates> {
   recurringInvoiceRefs: any;
@@ -205,19 +206,21 @@ export default class CreateRecurringInvoice extends Component<IProps, IStates> {
   fetchNextInvoice = () => {
     const {
       formValues: {starts_at, frequency},
-      dispatch
+      dispatch,
+      dateFormat
     } = this.props;
     const params = {
       starts_at,
       frequency
     };
-
     dispatch(
       fetchNextInvoiceAt({
         params,
         onSuccess: data => {
-          if (data?.success)
-            this.setFormField('next_invoice_at', data?.next_invoice_at);
+          if (data?.success) {
+            let nextInvDate = moment(data?.next_invoice_at).format(dateFormat);
+            this.setFormField('next_invoice_at', nextInvDate);
+          }
         }
       })
     );
@@ -237,6 +240,7 @@ export default class CreateRecurringInvoice extends Component<IProps, IStates> {
       }
     });
   };
+
   render() {
     const {
       isEditScreen,
