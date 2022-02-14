722a47eff217fbc8e196dc1611cb7656742e991f
KomelAbbas <komelabbasweb786@gmail.com>
2022-02-14 11:04:36 +0530

    update app version.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@
  * App Name:          Crater Invoice
  * Author:            Bytefury
  *
- * @since             5.0.1
+ * @since             6.0.2
  *
  * @format
  * @flow
--- config/index.js
+++ config/index.js
@@ -1,1 +1,1 @@
-export const APP_VERSION = '6.0.0';
+export const APP_VERSION = '6.0.2';
